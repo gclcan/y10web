ef36ef0b4549d44a679d2f0d705f8fa152966be6
Guclu Can <guclu.can@qwerty.local>
2019-01-26 02:17:24 -0500

    These are my changes

--- game.js
+++ game.js
@@ -40,10 +40,11 @@ window.onload = function() {
     for(var i=0; i<sprites.length; i++){
         sprites[i].onload = function(){
             loaded++;
+            if(loaded == 3){
+                run();
+            }
         }
-        if(loaded == 3){
-            run();
-        }
+        
     }
     
     
@@ -52,6 +53,8 @@ window.onload = function() {
         sprite:sprites[0], 
         x: width/2 -32,
         y: height - 32,
+        width:104,
+        height:32, 
         update: function(){
             if(key[RIGHT]){
                 this.x += 4;
@@ -60,11 +63,12 @@ window.onload = function() {
                 this.x -= 4;
             }
             if(this.x <= 0){this.x = 0;}
-            if(this.x >= width - 64){this.x = width - 64;}
+            if(this.x >= width - this.width){this.x = width - this.width;}
 
         },
         render:function(){
-            c.drawImage(this.sprite,this.x,this.y);
+            //c.strokeRect(this.x,this.y,64,16);
+          c.drawImage(this.sprite,this.x-64,this.y-16);
         }
     };
      
@@ -74,12 +78,13 @@ window.onload = function() {
         y:player.y-16,
         dx:2,
         dy:2,
-        ray:8,
+        width:22,
+        height:22,
         holding:true,
         update:function(){
             if(this.holding){
                 this.x = player.x+32;
-                this.y = player.y-16;
+                this.y = player.y-this.height-4 ;
                 if(key[SPACE]){
                     this.holding = false;
                 }
@@ -88,50 +93,57 @@ window.onload = function() {
                 this.x += this.dx;
                 this.y += this.dy;
                 //collision
-                if(this.y >= player.y-this.ray && (this.x >= player.x && this.x <= player.x+64) && this.y < player.y+32){
+                if(this.y >= player.y-this.height && (this.x >= player.x && this.x <= player.x+player.width) && this.y < player.y+player.height){
                     this.dy = -this.dy;
                 }
-                if(this.y <= this.ray){
+                if(this.y <= 0){
                     this.dy = -this.dy;
                 }
-                if(this.x <= -this.ray || this.x >= width-this.ray){
+                if(this.x <= 0 || this.x >= width-this.width ){
                     this.dx = -this.dx;
                 }
-                if(this.y > height + this.ray){
+                if(this.y > height + this.height ){
                     this.holding = true;
                     lives--;
                 }
             }
         },
         render:function(){
-            c.beginPath();
-                c.arc(this.x,this.y,8,0,Math.PI*2);
-            c.stroke();
+            c.drawImage(this.sprite,this.x,this.y);
+//            c.beginPath();
+//            c.arc(this.x,this.y,8,0,Math.PI*2);
+//            c.stroke();
         }
     }
     
     brick = [];
     for(var i=0; i<8; i++){
-        brick[i] = {
-            sprite:sprites[2],
-            x: 64+(i*64),
-            y : 32,
-            broken:false,
-            update:function(){
-                if(!this.broken){
-                    if(ball.y < this.y +32+ball.ray  && ball.x > this.x-ball.ray && ball.x < this.x + 64+ball.ray && ball.y > this.y - ball.ray){
-                    ball.dy = -ball.dy;
-                    this.broken = true;
-                        score += 10;
+        brick[i] = new Array();
+        for(var j=0; j<4; j++){
+        brick[i][j] = {
+                sprite:sprites[2],
+                x: 64+(i*64),
+                y : 32+(j*32),
+                width:64,
+                height:32, 
+                broken:false,
+                update:function(){
+                    if(!this.broken){
+                        if(ball.y < this.y +this.height && ball.x > this.x-ball.width && ball.x < this.x +  this.width+ball.width && ball.y > this.y - ball.height){
+                        ball.dy = -ball.dy;
+                        this.broken = true;
+                            score += 10;
+                        }
+                    }
+                },
+                render:function(){
+                    if(!this.broken){
+                        c.drawImage(this.sprite,this.x,this.y);
+                        //c.strokeRect(this.x,this.y,64,32);
                     }
                 }
-            },
-            render:function(){
-                if(!this.broken){
-                    c.strokeRect(this.x,this.y,64,32);
-                }
-            }
-        }
+          }
+       }
     }
     //
     gameScene = {
@@ -139,7 +151,8 @@ window.onload = function() {
               player.update();
         ball.update();
         for(var i=0; i<8; i++){
-            brick[i].update();
+            for(var j=0; j<4; j++){
+            brick[i][j].update();}
         }
         if(lives<=0){
             gameover = true;
@@ -156,7 +169,8 @@ window.onload = function() {
         player.render();
         ball.render();
         for(var i=0; i<8; i++){
-            brick[i].render();
+            for(var j=0; j<4; j++){
+            brick[i][j].render();}
         }
         c.fillText("Score : " + score,16,16);
         c.fillText("Lives : " + lives,16,32);
